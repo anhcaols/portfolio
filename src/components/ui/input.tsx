fb0debbffea7fd8255262764a2c38d518967c65b
anh cao <anhct@northstudio.vn>
2024-04-23 16:40:07 +0700

    :art: add ui component shadcn

---
+++ src/components/ui/input.tsx
@@ -0,0 +1,22 @@
+import * as React from "react";
+
+import { cn } from "@/libs/utils";
+
+export interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {}
+
+const Input = React.forwardRef<HTMLInputElement, InputProps>(({ className, type, ...props }, ref) => {
+  return (
+    <input
+      type={type}
+      className={cn(
+        "bg-transparent file:bg-transparent placeholder:text-gray border-gray flex h-9 w-full rounded-md border px-3 py-1  text-base hover:border-primary focus:border-primary focus:outline-none",
+        className
+      )}
+      ref={ref}
+      {...props}
+    />
+  );
+});
+Input.displayName = "Input";
+
+export { Input };
