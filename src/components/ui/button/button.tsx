71db18f6f06e22ca6251bebfd24cbe54f17436b6
anh cao <anhct@northstudio.vn>
2024-04-28 00:40:08 +0700

    :memo: customize button

--- src/components/ui/button/button.tsx
+++ src/components/ui/button/button.tsx
@@ -2,23 +2,24 @@ import * as React from "react";
 import { Slot } from "@radix-ui/react-slot";
 import { cva, type VariantProps } from "class-variance-authority";
 import { cn } from "@/lib/utils/cn";
+import styles from "./styles.module.scss";
 
 const buttonVariants = cva(
   "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50",
   {
     variants: {
       variant: {
-        default: "bg-primary text-light shadow hover:bg-primary/90",
-        outline: "border border-gray hover:border-primary hover:text-primary",
+        default: `bg-primary ${styles.buttonWrapper} ${styles.btnEffect}`,
+        outline: `${styles.buttonWrapper} ${styles.btnEffect}`,
         link: "text-primary underline-offset-4 hover:underline",
-        destructive: "bg-destructive text-light shadow hover:bg-destructive/90",
-        ghost: "bg-transparent text-light shadow hover:bg-hover",
-        icon: "bg-transparent text-light shadow hover:bg-primary"
+        destructive: "bg-destructive shadow hover:bg-destructive/90",
+        ghost: "bg-transparent shadow",
+        icon: "bg-transparent shadow hover:bg-primary"
       },
       size: {
-        default: "h-9 px-4 py-2 text-base",
-        sm: "h-8 rounded-md px-3 text-xs",
-        lg: "h-10 rounded-md px-8 text-xl"
+        sm: "h-8 rounded-md px-3 text-base !rounded-none",
+        default: `h-10 px-5 text-lg !rounded-none text-xl`,
+        lg: "h-12 rounded-md px-6 text-xl !rounded-none"
       }
     },
     defaultVariants: {
@@ -35,9 +36,16 @@ export interface ButtonProps
 }
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ className, variant, size, asChild = false, ...props }, ref) => {
+  ({ className, variant, size, asChild = false, children, ...props }, ref) => {
     const Comp = asChild ? Slot : "button";
-    return <Comp className={cn(buttonVariants({ variant, size, className }))} ref={ref} {...props} />;
+    return (
+      <div>
+        <Comp className={cn(buttonVariants({ variant, size, className }))} ref={ref} {...props}>
+          <span className={styles.borderFade}></span>
+          <span className={styles.effect}>{children}</span>
+        </Comp>
+      </div>
+    );
   }
 );
 Button.displayName = "Button";
