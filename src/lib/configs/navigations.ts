6c9ad4c3db7e23ccb03242c6bfed7615dbba4463
anh cao <anhct@northstudio.vn>
2024-04-29 17:01:13 +0700

    :memo: add configs

---
+++ src/lib/configs/navigations.ts
@@ -0,0 +1,27 @@
+export const NAVIGATION_CONFIGS = [
+  {
+    key: "home",
+    label: "Home",
+    path: "home"
+  },
+  {
+    key: "about",
+    label: "About",
+    path: "about"
+  },
+  {
+    key: "projects",
+    label: "Projects",
+    path: "projects"
+  },
+  {
+    key: "experience",
+    label: "Experience",
+    path: "experience"
+  },
+  {
+    key: "contact",
+    label: "Contact",
+    path: "contact"
+  }
+]
