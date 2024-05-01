8db0e3c3ca7b42492f1bd8b9e983fbf1c46cdeac
anh cao <anhct@northstudio.vn>
2024-05-01 13:04:26 +0700

    :memo: update header

--- src/layouts/MainLayout/Header/index.tsx
+++ src/layouts/MainLayout/Header/index.tsx
@@ -14,9 +14,10 @@ export default function Header() {
         <div className="hidden gap-3 lg:flex">
           {NAVIGATION_CONFIGS.map((item) => (
             <LinkS
+              key={item.key}
               className={styles.navItem}
               activeClass={styles.active}
-              to={item?.path}
+              to={item.path}
               spy
               smooth
               offset={-80}
